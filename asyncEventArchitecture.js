b7b6d547b209ff0b104b2296440e00197617100b
Solid Peace <55583106+Solid-Peace@users.noreply.github.com>
2020-09-24 11:30:33 +0200

    Add files via upload

---
+++ asyncEventArchitecture.js
@@ -0,0 +1,113 @@
+import axios from 'axios'
+import EventEmitter from 'events'
+
+
+class MyEmitter extends EventEmitter {
+    constructor(){
+        super()
+        this.process = {}
+        this.process.test1 = false
+        this.process.test2 = false
+    }
+}
+
+// MyEmitter.process.test1 = false
+// MyEmitter.process.test2 = false
+
+const myEmitter = new MyEmitter()
+
+
+myEmitter.on('launch', function(){
+    console.log(`test 1 == ${this.process.test1} \n test 2 == ${this.process.test2} \n system is launching`)
+})
+
+myEmitter.on('pending', function(){
+    (this.process.test1 == true && this.process.test2 == true) ?
+        (() => { 
+            console.log('ressources loading 2/2') 
+            this.emit('launch') 
+        })()
+        :
+        console.log('ressources loading 1/2') 
+})
+
+myEmitter.test1 = function() {
+    setTimeout(function(){
+        myEmitter.process.test1 = true
+        myEmitter.emit('pending')
+    }, 2000)
+}
+
+
+myEmitter.test2 = function() {
+    setTimeout(function(){
+        myEmitter.process.test2 = true
+        myEmitter.emit('pending')
+    }, 5000)
+}
+
+
+myEmitter.test1()
+myEmitter.test2()
+
+
+
+
+
+
+async function getContent() {
+    console.log('getContent middleware start')
+    let { data:CMS } = await axios.get('http://localhost:1337/pages')
+    console.log(CMS)
+}
+
+
+// (async function getContent() {
+//     console.log('getContent middleware start')
+//     let { data:CMS } = await axios.get('http://localhost:1337/pages')
+//     console.log(CMS)
+// })()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default async function({app}) {
+
+//     console.log(app)
+//     let menuItems = []
+//     let {data} = await app.$axios.get('http://localhost:1337/pages')
+    
+//     data.forEach(element => {
+//         menuItems.push(element.menuTitle)
+//     });
+
+//     // this.menuItems = menuItems
+//     console.log(app)
+// }
